872b9e7e36dc46421ec9140dc1dfd8d51447e835
christian-simmons <simmonschristianr@gmail.com>
2019-06-19 16:02:15 -0500

    added back new site

---
+++ public/fr/template/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135344506-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-135344506-1');
+  </script>
+    <title>Christian Simmons's Website</title>
+    <meta charset="utf-8">
+    <link href= ../css/main.css rel="stylesheet">
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+    </script>
+    <![endif]-->
+    <link rel="images/apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png">
+    <link rel="images/icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+    <link rel="images/icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+    <link rel="images/manifest" href="../images/site.webmanifest">
+    <link rel="images/mask-icon" href="../images/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body class="front">
+    <a href="index.php" id="logo"><img src="../images/logoFR.png" alt="Christian Simmons"></a>
+    <div id="nav">
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <hr width="50%" size="1" align="center" color="#1f2633">
+          <li><a href="portfolio.php">Dossier Professionnel</a></li>
+          <hr width="50%" size="1" align="center" color="#1f2633">
+          <li><a href="../ChristianSimmonsResume.pdf" id="resume-link" target="_blank">Resume</a></li>
+          <hr width="50%" size="1" align="center" color="#1f2633">
+          <li><a href="bio.php">Bio</a></li>
+          <hr width="50%" size="1" align="center" color="#1f2633">
+
